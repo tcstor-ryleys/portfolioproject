45b15b1ea988b11aff2a393143c78896ced1f4b0
kaylee <Kiwi.Sudduth@gmail.com>
2024-11-11 17:00:22 -0800

    worked with JS

---
+++ javascript practice/script.js
@@ -0,0 +1,16 @@
+console.log("meow");
+//window.alert("alert");
+document.getElementById("myh1").textContent = "meow";
+//document.getElementById("p1").textContent = "!!!";
+let fullname = "Ryley Blair Sudduth";
+let age = 17;
+let isStudent = true;
+document.getElementById("p1").textContent = `Your name is ${fullname}`;
+document.getElementById("p2").textContent = `Your age is ${age}`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+function catsss(){
+    console.log("meow");
+
+
+}
+catsss();
