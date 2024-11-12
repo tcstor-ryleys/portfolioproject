abfafac7af09aa00b3cd56fc816eb26a6185156d
armaghan <kiwi.sudduth@gmail.com>
2024-11-11 17:01:46 -0800

    js practice

---
+++ javascriptpractice/script.js
@@ -0,0 +1,13 @@
+console.log("e");
+//window.alert("alert!");
+document.getElementById("myh1").textContent = ":3";
+let fullname = "Kaylee Rhea Sudduth";
+let age = 17;
+let isstudent = true;
+document.getElementById("myp").textContent = `my name is ${fullname}`;
+document.getElementById("myp2").textContent = `my age is ${age}`;
+document.getElementById("myp3").textContent = `Enrolled ${isstudent}`;
+function aa(){
+    console.log("e");
+}
+aa();
