e78371e1ff0adeebdc74b53b58181d4f6fce43fe
rylee <rybear.sudduth@gmail.com>
2024-11-18 16:57:20 -0800

    worked with variables on JavaScript

--- javascript practice/script.js
+++ javascript practice/script.js
@@ -5,12 +5,33 @@ document.getElementById("myh1").textContent = "meow";
 let fullname = "Ryley Blair Sudduth";
 let age = 17;
 let isStudent = true;
-document.getElementById("p1").textContent = `Your name is ${fullname}`;
-document.getElementById("p2").textContent = `Your age is ${age}`;
-document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
-function catsss(){
-    console.log("meow");
-
-
-}
-catsss();
+let favoritecolor = "purple";
+let students = 30;
+//let username;
+// students = students + 1;
+// students = students - 1;
+// students = students ** 2;
+// students = students / 2;
+// students += 1;
+// students -= 1;
+// students **= 2;
+// students /= 2;
+// console.log(students);
+// console.log(typeof isStudent);
+// document.getElementById(
+//   "p4"
+// ).textContent = `Your favorite color is ${favoritecolor}`;
+// document.getElementById("p1").textContent = `Your name is ${fullname}`;
+// document.getElementById("p2").textContent = `Your age is ${age}`;
+// document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+// function catsss() {
+//   console.log("meow");
+// }
+// catsss();
+//easywaytogetusername
+// let username = window.prompt("What Is Your Username?");
+// console.log(username);
+document.getElementById("button").onclick = function getUserInput() {
+  username = document.getElementById("input").value;
+  document.getElementById("myh1").textContent = `Hello ${username}`;
+};
